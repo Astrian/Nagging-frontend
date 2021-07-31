86d8042a29b5525e42d8a7f6a4a02b8168cdc9ea
Astrian Zheng <git@astrianzheng.cn>
2021-07-31 13:54:04 +0800

    profile card

--- src/components/ProfileStyleA/index.js
+++ src/components/ProfileStyleA/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import {Row, Col} from 'react-bootstrap'
 import { gql } from "@apollo/client"
 import apollo from '../../extentions/apollo'
+import './index.scss'
 
 class ProfileStyleA extends React.Component {
   constructor(props) {
     super(props)
     this.state = {}
     this.state.user = {
-      avatar: `${process.env.PUBLIC_URL}/defaultavatar.jpg`
+      avatar: `${process.env.PUBLIC_URL}/defaultavatar.jpg`,
+      fullname: `John Appleseed`,
+      username: `Username`,
+      bio: ``
     }
   }
   componentDidMount() {
@@ -28,9 +33,13 @@ class ProfileStyleA extends React.Component {
   render() {
     console.log(this.state.user.avatar)
     return (
-      <div className='profile'>
-        <img className='avatar' alt='User avatar' src={this.state.user ? this.state.user.avatar : `${process.env.PUBLIC_URL}/defaultavatar.jpg`} />
-      </div>  
+      <Row className='justify-content-md-center'>
+        <Col lg="4" className='profile'>
+          <img className='avatar' alt='User avatar' src={this.state.user.avatar} />
+          <p className='fullname'>{this.state.user.fullname}</p>
+          <p className='bio'>{this.state.user.bio || `Yet another new Nagging space.`}</p>
+        </Col>
+      </Row>  
     )
   }
 }
