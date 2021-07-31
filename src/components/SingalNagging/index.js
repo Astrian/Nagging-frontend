79de6eba8f0f8e6e602094d805bcc4956da045bd
Astrian Zheng <git@astrianzheng.cn>
2021-07-31 18:00:12 +0800

    new component for display singal nagging

--- src/components/SingalNagging/index.js
+++ src/components/SingalNagging/index.js
@@ -1,23 +1,38 @@
 import React from 'react'
 import { withRouter } from "react-router-dom";
+import apollo from '../../extentions/apollo'
+import { gql } from "@apollo/client"
+import {Row, Col} from 'react-bootstrap'
+import Moment from 'react-moment'
+import './index.scss'
 
 class SingalNagging extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      uuid: this.props.match.params.uuid,
-      nagging: {
-        content: 'This is a nagging.',
-        time: Date.parse(new Date()),
-        uuid: this.props.match.params.uuid
-      }
+      content: 'This is a nagging.',
+      time: Date.parse(new Date()),
+      uuid: this.props.match.params.uuid
     }
   }
   componentDidMount() {
-    console.log(this.props.match.params)
+    apollo.query({ query: gql`
+    query singalNagging {
+      signalNagging(uuid: "${this.props.match.params.uuid}") {
+        uuid,
+        content,
+        time
+      }
+    }
+    ` }).then(res => this.setState({uuid: res.data.signalNagging.uuid, content: res.data.signalNagging.content, time: res.data.signalNagging.time }))
   }
   render() {
-    return (<p>The uuid is: {this.state.uuid}</p>)
+    return (<Row className='justify-content-md-center'>
+      <Col xl="6" className='singalNagging'>
+        <p class='content'>{this.state.content}</p>
+        <Moment format='YYYY/MM/DD HH:mm'>{this.state.time}</Moment>
+      </Col>
+    </Row>)
   }
 }
 
