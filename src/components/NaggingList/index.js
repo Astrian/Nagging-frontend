3ebb85b3e544eead4db1d410dc256e645e5bbb9d
Astrian Zheng <git@astrianzheng.cn>
2021-07-31 15:35:48 +0800

    Add pager function to nagging list component

--- src/components/NaggingList/index.js
+++ src/components/NaggingList/index.js
@@ -12,27 +12,16 @@ import {
 class NaggingList extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { list: [], listNext: false, loading: true }
+    this.state = { list: [], listNext: false, loading: true, pager: 0 }
   }
   componentDidMount() {
     // Fetch naggings list
-    apollo.query({ query: gql`
-      query naggings{
-        naggings {
-          list{
-            uuid,
-            content,
-            time
-          },
-          next
-        }
-      }
-    `}).then(res => this.setState({ list: res.data.naggings.list, listNext: res.data.naggings.next, loading: false }))
+    this.fetchNaggings()
   }
   render() {
     let naggingList = this.state.list.map((nagging) => {
       return (
-        <div className='naggingCell'>
+        <div className='naggingCell' key={nagging.uuid}>
           <Link className='timestamp' to={`/naggings/${nagging.uuid}`}><Moment fromNow>{nagging.time}</Moment></Link>
           <div className='content'>{nagging.content}</div>
         </div>
@@ -41,12 +30,31 @@ class NaggingList extends React.Component {
     let next = (<div></div>)
     if (!this.state.loading) {
       if (this.state.listNext) {
-        next = (<div className='next'>Load more</div>)
+        next = (<div className='next'><span className='nextTrigger' onClick={this.fetchNaggings}>Click here to load more</span></div>)
       } else {
-        next = (<div className='next end'>That's all naggings.</div>)
+        next = (<div className='next end'>That's all.</div>)
       }
     } else {
-      next = (<div className='next'>...</div>)
+      next = (<div className='next loading'>...</div>)
+    }
+    this.fetchNaggings = () => {
+      this.setState({ loading: true })
+      let list = this.state.list
+      let pager = this.state.pager
+      apollo.query({ query: gql`
+      query naggings{
+        naggings(pager: ${pager}){
+          list{
+            uuid,
+            content,
+            time
+          },
+          next
+        }
+      }
+      `}).then(res => {
+        this.setState({ list: list.concat(res.data.naggings.list), listNext: res.data.naggings.next, loading: false, pager: pager + 1 })
+      })
     }
     return (
       <Router>
