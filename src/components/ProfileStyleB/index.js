d036480035f0e26ea69a5342bcb6bd0d50c6e367
Astrian Zheng <git@astrianzheng.cn>
2021-08-02 12:46:13 +0800

    Change ProfileStyleB into function component

--- src/components/ProfileStyleB/index.js
+++ src/components/ProfileStyleB/index.js
@@ -1,51 +1,45 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Row, Col} from 'react-bootstrap'
-import { gql } from "@apollo/client"
-import apollo from '../../extentions/apollo'
+import { gql, useQuery } from "@apollo/client"
 import { Link } from "react-router-dom";
 import './index.scss'
 import { toast } from "react-toastify"
 
-class ProfileStyleB extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
-    this.state.user = {
-      avatar: `${process.env.PUBLIC_URL}/defaultavatar.jpg`,
-      fullname: `John Appleseed`,
-      username: `Username`,
-      bio: ``,
-      naggingCount: 0
-    }
+const FETCH_USER = gql`query user {
+  user {
+    uuid,
+    username,
+    fullname,
+    bio,
+    avatar,
+    naggingCount
   }
-  componentDidMount() {
-    // Fetch naggings list
-    apollo.query({ query: gql`
-      query user {
-        user {
-          uuid,
-          username,
-          fullname,
-          bio,
-          avatar,
-          naggingCount
-        }
-      }
-    `}).then(res => this.setState({user: res.data.user}))
-    .catch(e => toast(`Error occured when fetching profile: ${e.message}`))
-  }
-  render() {
-    return (
-      <Row className='justify-content-md-center'>
-        <Col lg="6" className='profileStyleB'>
-          <img className='avatar' alt='User avatar' src={this.state.user.avatar} />
-          <p className='authorTag'>Author</p>
-          <p className='primary'>{this.state.user.fullname}</p>
-          <p className='secondary'>{this.state.user.bio || `Yet another new Nagging space.`}<br /><Link to='/'>Checkout all naggings</Link></p>
-        </Col>
-      </Row>  
-    )
-  }
+}`
+
+function ProfileStyleB() {
+  const user = useState({
+    avatar: `${process.env.PUBLIC_URL}/defaultavatar.jpg`,
+    fullname: `John Appleseed`,
+    username: `Username`,
+    bio: ``,
+    naggingCount: 0
+  })
+  useQuery(FETCH_USER, {
+    onCompleted: e => {
+      user[1](e.user)
+    }, onError: e => {
+      toast(`Cannot fetch user profile: ${e.message}`)
+    }})
+  return (
+    <Row className='justify-content-md-center'>
+      <Col lg="6" className='profileStyleB'>
+        <img className='avatar' alt='User avatar' src={user[0].avatar} />
+        <p className='authorTag'>Author</p>
+        <p className='primary'>{user[0].fullname}</p>
+        <p className='secondary'>{user[0].bio || `Yet another new Nagging space.`}<br /><Link to='/'>Checkout all naggings</Link></p>
+      </Col>
+    </Row>  
+  )
 }
 
 export default ProfileStyleB
